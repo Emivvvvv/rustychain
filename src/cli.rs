32c15b442706e8e747b3c10744faa6351daeb71f
Emirhan TALA <emirhantala99ff99@gmail.com>
2023-08-06 01:22:33 +0300

    im sleepy

--- src/cli.rs
+++ src/cli.rs
@@ -1,4 +1,15 @@
 use clap;
+use clap::ArgMatches;
+
+pub struct Args {
+    private_key: u128,
+    app_mode: AppMode
+}
+
+pub enum AppMode {
+    Miner(u8),
+    Node,
+}
 
 /// Runs the clap app in order to use cli
 pub fn cli() -> clap::Command {
@@ -7,31 +18,62 @@ pub fn cli() -> clap::Command {
         .about(env!("CARGO_PKG_DESCRIPTION"))
         .arg(
             clap::Arg::new("private-key")
-                .required_unless_present_any(["node", "dev"])
+                .required_unless_present("node")
                 .index(1)
                 .help("Your private key."),
         )
         .arg(
             clap::Arg::new("miner")
                 .conflicts_with("node")
-                .required_unless_present_any(["node", "dev"])
+                .required_unless_present("node")
                 .short('m')
                 .long("miner")
                 .action(clap::ArgAction::SetTrue)
-                .help("Opens rustychain as a miner. To mine blocks use this flag.")
+                .help("Opens rustychain as a miner. To mine blocks use this flag. You can send transactions while mining.")
+        )
+        .arg(
+            clap::Arg::new("threads")
+                .conflicts_with("node")
+                .short('t')
+                .long("threads")
+                .default_value("8")
+                .help("Number of threads to be used."),
         )
         .arg(
             clap::Arg::new("node")
                 .short('n')
                 .long("node")
                 .action(clap::ArgAction::SetTrue)
-                .help("Opens rustychain as a node. You can only watch and save the blockchain data. You can make queries on blockchain.")
+                .help("Opens rustychain as a node. You can only watch and send transactions to the network.")
         )
-        .arg(
-            clap::Arg::new("dev")
-                .short('d')
-                .long("dev")
-                .action(clap::ArgAction::SetTrue)
-                .help("Dev mode for testing.")
-        )
+}
+
+impl Args {
+    pub fn get_args(matches: ArgMatches) -> Self{
+        let private_key_string = match matches.get_one::<String>("private-key") {
+            Some(pk) => pk.to_owned(),
+            None => String::new()
+        };
+
+        let threads = matches.get_one::<String>("threads")
+            .expect("This was unexpected :(. Something went wrong while getting -t or --threads arg")
+            .trim().parse::<u8>()
+            .expect("Threads must be a number!");
+
+        let app_mode = if matches.get_flag("miner") {AppMode::Miner(threads)}
+        else {AppMode::Node};
+
+        Args {
+            private_key: private_key_string.parse::<u128>().expect("private key must be u128 binary format(for now)"),
+            app_mode
+        }
+    }
+
+    pub fn get_private_key(&self) -> u128 {
+        self.private_key
+    }
+
+    pub fn get_app_mode(&self) -> &AppMode {
+        &self.app_mode
+    }
 }
--- src/miner.rs
+++ src/miner.rs
@@ -3,15 +3,18 @@ use crate::error::MinerError;
 
 pub struct Miner{
     miner_address: u128,
+    threads: u8,
 }
 
 impl Miner {
-    pub fn new(private_key: u128) -> Self {
+    pub fn new(private_key: u128, threads: u8) -> Self {
         Miner {
-            miner_address: Sha128::address(private_key)
+            miner_address: Sha128::address(private_key),
+            threads
         }
     }
 
-    fn mine_curr_block(&self) -> Result<(), MinerError> {
-    }
+    // fn mine_curr_block(&self) -> Result<(), MinerError> {
+    //
+    // }
 }
